6966381a33d360a3eb14499e1494133f6780f1a0
Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
2020-10-30 10:55:03 +0100

    add tracing

--- src/main/kotlin/no/nav/dagpenger/quizshow/api/SøknadApi.kt
+++ src/main/kotlin/no/nav/dagpenger/quizshow/api/SøknadApi.kt
@@ -7,11 +7,15 @@ import io.ktor.http.cio.websocket.readText
 import io.ktor.routing.routing
 import io.ktor.websocket.WebSockets
 import io.ktor.websocket.webSocket
+import mu.KotlinLogging
+
+val logger = KotlinLogging.logger {}
 
 internal fun Application.søknadApi() {
     install(WebSockets)
 
     routing {
+        trace { logger.info { it.buildText() } }
         webSocket("/chat") {
             while (true) {
                 val frame = incoming.receive() // suspend
