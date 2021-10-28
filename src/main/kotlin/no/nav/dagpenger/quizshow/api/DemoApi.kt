ed4e90b9a139543bfbe7bfec5069b24ca36d73a3
Geir André Lund <geir.andre.lund@nav.no>
2021-10-28 08:20:55 +0200

    Get test

--- src/main/kotlin/no/nav/dagpenger/quizshow/api/DemoApi.kt
+++ src/main/kotlin/no/nav/dagpenger/quizshow/api/DemoApi.kt
@@ -5,7 +5,9 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.StatusPages
 import io.ktor.response.respond
+import io.ktor.routing.get
 import io.ktor.routing.put
+import io.ktor.routing.route
 import io.ktor.routing.routing
 
 internal fun Application.demoApi(publiser: (String) -> Unit, env: Map<String, String>) {
@@ -13,11 +15,16 @@ internal fun Application.demoApi(publiser: (String) -> Unit, env: Map<String, St
     install(StatusPages)
 
     routing {
-        put(path = "/demo/{fnr}") {
-            assertNotProd(env)
-            val fnr = this.call.parameters["fnr"] ?: throw IllegalArgumentException("Må sette parameter til fnr")
-            publiser(fnr)
-            call.respond("$fnr publisert til rapid")
+        route("demo") {
+            put(path = "{fnr}") {
+                assertNotProd(env)
+                val fnr = this.call.parameters["fnr"] ?: throw IllegalArgumentException("Må sette parameter til fnr")
+                publiser(fnr)
+                call.respond("$fnr publisert til rapid")
+            }
+            get("ping") {
+                call.respond("pong!")
+            }
         }
     }
 }
