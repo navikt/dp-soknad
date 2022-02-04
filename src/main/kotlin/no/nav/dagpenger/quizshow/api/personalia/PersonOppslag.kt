e92b90c806d7c9c74f75e3683cc06fe003d6777a
Giao The Cung <giaothe@gmail.com>
2022-02-04 15:33:28 +0100

    Old school litt logging
    DAG-34

--- src/main/kotlin/no/nav/dagpenger/quizshow/api/personalia/PersonOppslag.kt
+++ src/main/kotlin/no/nav/dagpenger/quizshow/api/personalia/PersonOppslag.kt
@@ -1,9 +1,12 @@
 package no.nav.dagpenger.quizshow.api.personalia
 
 import io.ktor.http.HttpHeaders
+import mu.KotlinLogging
 import no.nav.dagpenger.pdl.PersonOppslag
 import no.nav.dagpenger.quizshow.api.Configuration
 import no.nav.dagpenger.quizshow.api.Configuration.tokenXClient
+
+private val logger = KotlinLogging.logger {}
 
 internal class PersonOppslag(
     private val personOppslag: PersonOppslag,
@@ -18,7 +21,9 @@ internal class PersonOppslag(
             mapOf(
                 HttpHeaders.Authorization to "Bearer ${tokenProvider.invoke(subjectToken, pdlAudience)}"
             )
-        )
+        ).also {
+            logger.info { "Fikk hentet person: $it" }
+        }
         val adresse = Adresse(
             adresselinje1 = "adresselinje1",
             adresselinje2 = "adresselinje2",
