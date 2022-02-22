99137365bd041db9b4d614c336af62b5eea34c61
Geir André Lund <geir.andre.lund@nav.no>
2022-02-22 14:12:33 +0100

    Gi 'cloudsqliamuser' rollen tilgang til tabeller DAG-135

---
+++ src/main/resources/db/migration/R__USER_privilieges.sql
@@ -0,0 +1,10 @@
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
+        THEN
+            GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+        END IF;
+    END
+$$;
