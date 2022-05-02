11aaa835cfa1998e5f7e3eb87c392bc871cd3adf
Geir André Lund <geir.andre.lund@nav.no>
2022-05-02 15:36:31 +0200

    Slette gammel mal for fikse hickup

--- mediator/src/main/resources/db/migration/R__USER_privilieges.sql
+++ mediator/src/main/resources/db/migration/R__USER_privilieges.sql
@@ -6,6 +6,7 @@ $$
             (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
         THEN
             GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+            GRANT ALL ON TABLE soknadmal TO cloudsqliamuser;
         END IF;
     END
 $$;
