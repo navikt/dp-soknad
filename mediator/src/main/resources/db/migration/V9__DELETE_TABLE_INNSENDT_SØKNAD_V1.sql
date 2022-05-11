808d0c3dd95d72c68cb138344f520677fd0b9ccb
Giao The Cung <giaothe@gmail.com>
2022-05-11 11:07:41 +0200

    Fjerne innsendt søknad tabell
    Co-authored-by: rannveigskjerve <rannveig.alette.skjerve@nav.no>

---
+++ mediator/src/main/resources/db/migration/V9__DELETE_TABLE_INNSENDT_SØKNAD_V1.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS innsendt_soknad_v1;
--- mediator/src/test/kotlin/no/nav/dagpenger/soknad/db/PostgresMigrationTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/soknad/db/PostgresMigrationTest.kt
@@ -10,7 +10,7 @@ class PostgresMigrationTest {
     fun `Migration scripts are applied successfully`() {
         withCleanDb {
             val migrations = runMigration()
-            assertEquals(9, migrations)
+            assertEquals(10, migrations)
         }
     }
 }
