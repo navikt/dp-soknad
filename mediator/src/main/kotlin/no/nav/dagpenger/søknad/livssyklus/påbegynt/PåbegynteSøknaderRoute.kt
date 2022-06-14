1baf2af82edf76b3caf04230f8d9713f6689806f
Marius Eriksen <marius.eriksen@nav.no>
2022-06-14 10:28:59 +0200

    Midlertidlig logg feil fra paabegynte endepunkt

--- mediator/src/main/kotlin/no/nav/dagpenger/søknad/livssyklus/påbegynt/PåbegynteSøknaderRoute.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/søknad/livssyklus/påbegynt/PåbegynteSøknaderRoute.kt
@@ -5,17 +5,25 @@ import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import mu.KotlinLogging
 import no.nav.dagpenger.søknad.SøknadMediator
 import no.nav.dagpenger.søknad.utils.auth.ident
 
+private val logger = KotlinLogging.logger {}
+
 internal fun Route.påbegyntSøknadRoute(søknadMediator: SøknadMediator) {
     get("/paabegynt") {
-        val påbegyntSøknad = søknadMediator.hentPåbegyntSøknad(call.ident())
+        try {
+            val påbegyntSøknad = søknadMediator.hentPåbegyntSøknad(call.ident())
 
-        if (påbegyntSøknad != null) {
-            call.respond(HttpStatusCode.OK, påbegyntSøknad)
-        } else {
-            call.respond(HttpStatusCode.NotFound)
+            if (påbegyntSøknad != null) {
+                logger.info { "$påbegyntSøknad returnert" }
+                call.respond(HttpStatusCode.OK, påbegyntSøknad)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        } catch (e: Exception) {
+            logger.error { "Noe gikk galt med henting av påbegynt søknad: $e" }
         }
     }
 }
