422a234c3743e1a7f583c4d484989c9103bec6ba
Celine ten Dam <celine.ten.dam@nav.no>
2022-06-22 09:20:00 +0200

    Gir alle tilgang til cloudsqlamuser for å slette søknadsmal.
    Co-authored-by: Geir Andre Lund <geir.andre.lund@nav.no>
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- mediator/src/main/resources/db/migration/R__USER_privilieges.sql
+++ mediator/src/main/resources/db/migration/R__USER_privilieges.sql
@@ -5,7 +5,7 @@ $$
         IF EXISTS
             (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
         THEN
-            GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+            GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
         END IF;
     END
 $$;
