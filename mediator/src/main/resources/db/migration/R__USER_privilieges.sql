3cc4150a06c785f259176cc740f97a35394c8018
Celine ten Dam <celine.ten.dam@nav.no>
2022-06-22 09:50:32 +0200

    Rydd i tilganger før vi legger til spesifikke tilganger.
    Co-authored-by: Geir Andre Lund <geir.andre.lund@nav.no>
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- mediator/src/main/resources/db/migration/R__USER_privilieges.sql
+++ mediator/src/main/resources/db/migration/R__USER_privilieges.sql
@@ -5,6 +5,7 @@ $$
         IF EXISTS
             (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
         THEN
+            REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
             GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
         END IF;
     END
