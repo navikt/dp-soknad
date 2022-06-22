31a7fcfeb0675c424126c778a7771f6c32838e67
Celine ten Dam <celine.ten.dam@nav.no>
2022-06-22 09:27:40 +0200

    Revert "Gir alle tilgang til cloudsqlamuser for å slette søknadsmal."
    This reverts commit 422a234c3743e1a7f583c4d484989c9103bec6ba.

--- mediator/src/main/resources/db/migration/R__USER_privilieges.sql
+++ mediator/src/main/resources/db/migration/R__USER_privilieges.sql
@@ -5,7 +5,7 @@ $$
         IF EXISTS
             (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
         THEN
-            GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+            GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
         END IF;
     END
 $$;
