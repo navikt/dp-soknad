86e34e1dc94aa1b7a5c994e52b113f5ac5fb7e1d
Marius Eriksen <marius.eriksen@nav.no>
2022-09-13 10:54:50 +0200

    F legger til ident på søknad_slettet event
    DAG-306
    
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/observers/SøknadSlettetObserver.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/observers/SøknadSlettetObserver.kt
@@ -4,17 +4,17 @@ import no.nav.dagpenger.soknad.SøknadObserver
 import no.nav.dagpenger.soknad.SøknadObserver.SøknadSlettetEvent
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidsConnection
-import java.util.UUID
 
 internal class SøknadSlettetObserver(private val rapidsConnection: RapidsConnection) : SøknadObserver {
 
     override fun søknadSlettet(event: SøknadSlettetEvent) =
-        rapidsConnection.publish(event.ident, søknadSlettetEvent(event.søknadId))
+        rapidsConnection.publish(event.ident, søknadSlettetEvent(event))
 
-    private fun søknadSlettetEvent(søknadUuid: UUID) = JsonMessage.newMessage(
+    private fun søknadSlettetEvent(event: SøknadSlettetEvent) = JsonMessage.newMessage(
         eventName = "søknad_slettet",
         map = mapOf(
-            "søknad_uuid" to søknadUuid,
+            "søknad_uuid" to event.søknadId,
+            "ident" to event.ident,
         )
     ).toJson()
 }
