f819c271c8927c26222493116c865b2c245c8f05
Giao The Cung <giaothe@gmail.com>
2022-11-09 14:33:24 +0100

    Revert "TEMP: SKIP ALL"
    This reverts commit c1465d2fc3c116841a244b2477455389e8e59f09.

--- mediator/src/test/kotlin/no/nav/dagpenger/soknad/livssyklus/påbegynt/SøkerOppgaveMottakTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/soknad/livssyklus/påbegynt/SøkerOppgaveMottakTest.kt
@@ -11,7 +11,6 @@ import no.nav.dagpenger.soknad.utils.db.PostgresDataSourceBuilder
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -28,7 +27,6 @@ class SøkerOppgaveMottakTest {
     }
 
     @Test
-    @Disabled
     fun `lese svar fra kafka`() {
         Postgres.withMigratedDb {
             val søknadMediator = SøknadMediator(
