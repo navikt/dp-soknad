204ce1609ef4d9e4d53e95d1d51eee2d2076b060
André Roaldseth <andre.roaldseth@nav.no>
2022-11-10 14:31:03 +0100

    Logg hvor mange observers som varsles om ny mal
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/mal/SøknadMalRepository.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/mal/SøknadMalRepository.kt
@@ -6,6 +6,7 @@ import kotliquery.Session
 import kotliquery.queryOf
 import kotliquery.sessionOf
 import kotliquery.using
+import mu.KotlinLogging
 import no.nav.dagpenger.soknad.Prosessnavn
 import no.nav.dagpenger.soknad.Prosessversjon
 import no.nav.dagpenger.soknad.mal.SøknadMalRepository.SøknadMalObserver
@@ -43,7 +44,9 @@ class SøknadMalPostgresRepository(private val dataSource: DataSource) : Søknad
             )
         }
     }.also { nyMal ->
-        if (nyMal == 1) observers.forEach { it.nyMal(søknadMal) }
+        if (nyMal == 1) observers.forEach { it.nyMal(søknadMal) }.also {
+            logger.info { "Varsler ${observers.size} om ny mal" }
+        }
     }
 
     override fun hentNyesteMal(prosessnavn: Prosessnavn): SøknadMal = using(sessionOf(dataSource)) { session: Session ->
@@ -91,8 +94,9 @@ class SøknadMalPostgresRepository(private val dataSource: DataSource) : Søknad
             )
         } ?: throw IllegalArgumentException("Kjenner ikke til prosessnavn=$prosessnavn, prosessversjon=$versjon")
 
-    companion object {
+    private companion object {
         private val objectMapper = jacksonObjectMapper()
+        private val logger = KotlinLogging.logger {}
     }
 
     override fun addObserver(søknadMalObserver: SøknadMalObserver) = observers.add(søknadMalObserver)
