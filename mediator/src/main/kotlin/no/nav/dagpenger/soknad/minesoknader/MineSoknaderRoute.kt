d446ab110ba9b0d197f1ca9af697137ba8246d22
celinetendam <celine.ten.dam@nav.no>
2023-02-02 14:59:14 +0100

    Henter ut dokumentkrav for innsendte søknader ved bruk av query param include=dokumentkrav

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/minesoknader/MineSoknaderRoute.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/minesoknader/MineSoknaderRoute.kt
@@ -7,6 +7,7 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import mu.KotlinLogging
+import no.nav.dagpenger.soknad.Krav
 import no.nav.dagpenger.soknad.Søknad
 import no.nav.dagpenger.soknad.Søknad.Companion.erDagpenger
 import no.nav.dagpenger.soknad.Søknad.Tilstand.Type.Innsendt
@@ -23,20 +24,25 @@ private val logger = KotlinLogging.logger { }
 internal fun Route.mineSoknaderRoute(søknadMediator: SøknadMediator) {
     get("/mine-soknader") {
         val fom = queryParamToFom(call.request.queryParameters["fom"])
+        val includeDokumentkrav = call.request.queryParameters["include"] == "dokumentkrav"
 
         val dagpengeSøknader = søknadMediator.hentSøknader(ident = call.ident()).filter { it.erDagpenger() }
-        val mineSøknaderDto = lagMineSøknaderDto(dagpengeSøknader, fom)
+        val mineSøknaderDto = lagMineSøknaderDto(dagpengeSøknader, fom, includeDokumentkrav)
 
         call.respond(HttpStatusCode.OK, mineSøknaderDto)
     }
 }
 
-private fun lagMineSøknaderDto(søknader: List<Søknad>, fom: LocalDate): MineSoknaderDto {
+private fun lagMineSøknaderDto(søknader: List<Søknad>, fom: LocalDate, includeDokumentkrav: Boolean): MineSoknaderDto {
     var påbegyntSøknad: PåbegyntSøknadDto? = null
     val innsendteSøknader = mutableListOf<InnsendtSøknadDto>()
 
     søknader.map { søknad ->
         val mineSøknaderVisitor = MineSøknaderVisitor(søknad)
+        val dokumentkrav = if (includeDokumentkrav) {
+            mineSøknaderVisitor.dokumentkrav().toMineSoknaderDokumentkravDTO()
+        } else null
+
         when {
             mineSøknaderVisitor.søknadTilstand() == Påbegynt ->
                 påbegyntSøknad = PåbegyntSøknadDto(
@@ -48,7 +54,11 @@ private fun lagMineSøknaderDto(søknader: List<Søknad>, fom: LocalDate): MineS
             mineSøknaderVisitor.søknadTilstand() == Innsendt -> {
                 if (mineSøknaderVisitor.søknadInnsendt().toLocalDate() > fom) {
                     innsendteSøknader.add(
-                        InnsendtSøknadDto(søknad.søknadUUID(), mineSøknaderVisitor.søknadInnsendt())
+                        InnsendtSøknadDto(
+                            søknad.søknadUUID(),
+                            mineSøknaderVisitor.søknadInnsendt(),
+                            dokumentkrav
+                        )
                     )
                 }
             }
@@ -59,11 +69,39 @@ private fun lagMineSøknaderDto(søknader: List<Søknad>, fom: LocalDate): MineS
     return MineSoknaderDto(påbegyntSøknad, innsendteSøknader.ifEmpty { null })
 }
 
-data class MineSoknaderDto(val paabegynt: PåbegyntSøknadDto?, val innsendte: List<InnsendtSøknadDto>?)
+internal fun Set<Krav>.toMineSoknaderDokumentkravDTO(): List<MineSoknaderDokumentkravDTO> = map { krav ->
+    MineSoknaderDokumentkravDTO(
+        id = krav.id,
+        beskrivendeId = krav.beskrivendeId,
+        begrunnelse = krav.svar.begrunnelse,
+        filer = krav.svar.filer.map { it.filnavn },
+        valg = krav.svar.valg.name,
+        bundleFilsti = krav.svar.bundle?.namespaceSpecificString()?.toString()
+    )
+}
 
-data class PåbegyntSøknadDto(val soknadUuid: UUID, val opprettet: LocalDateTime, val sistEndretAvBruker: LocalDateTime?)
+internal data class MineSoknaderDto(val paabegynt: PåbegyntSøknadDto?, val innsendte: List<InnsendtSøknadDto>?)
 
-data class InnsendtSøknadDto(val soknadUuid: UUID, val forstInnsendt: LocalDateTime)
+internal data class PåbegyntSøknadDto(
+    val soknadUuid: UUID,
+    val opprettet: LocalDateTime,
+    val sistEndretAvBruker: LocalDateTime?,
+)
+
+internal data class InnsendtSøknadDto(
+    val soknadUuid: UUID,
+    val forstInnsendt: LocalDateTime,
+    val dokumentkrav: List<MineSoknaderDokumentkravDTO>?,
+)
+
+internal data class MineSoknaderDokumentkravDTO(
+    val id: String,
+    val beskrivendeId: String,
+    val begrunnelse: String?,
+    val filer: List<String>,
+    val valg: String,
+    val bundleFilsti: String?,
+)
 
 private fun queryParamToFom(param: String?): LocalDate {
     if (param == null) {
