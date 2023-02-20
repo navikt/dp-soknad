8b54cb8a29bf439616f8df82ab0232b9a4d7b511
celinetendam <celine.ten.dam@nav.no>
2023-02-20 09:53:09 +0100

    Legger til en todo
    Co-authored-by: Giao The Cung<giaothe@gmail.com>

--- modell/src/test/kotlin/no/nav/dagpenger/soknad/SøknadDokumentasjonskravTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/soknad/SøknadDokumentasjonskravTest.kt
@@ -17,6 +17,7 @@ class SøknadDokumentasjonskravTest {
         sannsynliggjør = faktaSomSannsynliggjøres
     )
 
+    //TODO: vurder å fjerne
 //    @Test
 //    fun `håndter svar på dokumentasjonskrav`() {
 //        val søknadId = UUID.randomUUID()
