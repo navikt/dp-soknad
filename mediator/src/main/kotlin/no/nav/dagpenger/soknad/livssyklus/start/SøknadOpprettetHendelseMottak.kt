a9edc36fcd9ed7617a2347e59382b2717788827f
André Roaldseth <andre.roaldseth@nav.no>
2023-03-08 12:42:19 +0100

    Hopp over SøknadOpprettet uten prosessversjon
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
    Co-authored-by: Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/livssyklus/start/SøknadOpprettetHendelseMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/livssyklus/start/SøknadOpprettetHendelseMottak.kt
@@ -11,14 +11,18 @@ import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import java.util.UUID
 
 internal class SøknadOpprettetHendelseMottak(
     rapidsConnection: RapidsConnection,
-    private val mediator: SøknadMediator
+    private val mediator: SøknadMediator,
 ) : River.PacketListener {
     companion object {
         private val logger = KotlinLogging.logger {}
         private val sikkerLogger = KotlinLogging.logger("tjenestekall.SøknadOpprettetHendelseMottak")
+        private val skipBehov = listOf(
+            "e8c60283-006e-439e-90d2-ffdfd533072f",
+        ).map { UUID.fromString(it) }
     }
 
     private val behov = NySøknad.name
@@ -34,15 +38,18 @@ internal class SøknadOpprettetHendelseMottak(
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         val behov = packet["@behov"].single().asText()
+        val behovId = packet["@behovId"].asUUID()
         val søknadID = packet["søknad_uuid"].asUUID()
 
+        if (behovId in skipBehov) return
+
         withLoggingContext(
-            "søknadId" to søknadID.toString()
+            "søknadId" to søknadID.toString(),
         ) {
             val prosessversjon = try {
                 mediator.prosessversjon(
                     packet["@løsning"][behov]["prosessversjon"]["prosessnavn"].asText(),
-                    packet["@løsning"][behov]["prosessversjon"]["versjon"].asInt()
+                    packet["@løsning"][behov]["prosessversjon"]["versjon"].asInt(),
                 )
             } catch (e: Exception) {
                 logger.warn(e) { "Kunne ikke finne prosessversjon i pakken (se sikkerlogg)" }
