c4ef11919f97403752ad4dfb4755cde56555c91d
André Roaldseth <andre.roaldseth@nav.no>
2023-05-24 11:30:45 +0200

    Fortsett å lese inn journalføringshendelser

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/Configuration.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/Configuration.kt
@@ -10,28 +10,23 @@ import no.nav.dagpenger.oauth2.CachedOauth2Client
 import no.nav.dagpenger.oauth2.OAuth2Config
 
 internal object Configuration {
-
     const val appName = "dp-soknad"
-
     private val defaultProperties = ConfigurationMap(
         mapOf(
             "RAPID_APP_NAME" to "dp-soknad",
             "KAFKA_CONSUMER_GROUP_ID" to "dp-soknad-v1",
             "KAFKA_RAPID_TOPIC" to "teamdagpenger.rapid.v1",
-            // "KAFKA_EXTRA_TOPIC" to "teamdagpenger.journalforing.v1",
+            "KAFKA_EXTRA_TOPIC" to "teamdagpenger.journalforing.v1",
             "KAFKA_RESET_POLICY" to "latest",
-            "PERSON_KONTO_REGISTER_URL" to "http://sokos-kontoregister-person.okonomi/api/borger/v1/hent-aktiv-konto"
-        )
+            "PERSON_KONTO_REGISTER_URL" to "http://sokos-kontoregister-person.okonomi/api/borger/v1/hent-aktiv-konto",
+        ),
     )
     val properties =
         ConfigurationProperties.systemProperties() overriding EnvironmentVariables() overriding defaultProperties
-
     val config: Map<String, String> = properties.list().reversed().fold(emptyMap()) { map, pair ->
         map + pair.second
     }
-
     val basePath = "/arbeid/dagpenger/soknadapi"
-
     val kontoRegisterUrl by lazy { properties[Key("PERSON_KONTO_REGISTER_URL", stringType)] }
     val kontoRegisterScope by lazy { properties[Key("PERSON_KONTO_REGISTER_SCOPE", stringType)] }
     val pdlUrl by lazy {
@@ -42,18 +37,18 @@ internal object Configuration {
     val pdlAudience by lazy { properties[Key("PDL_AUDIENCE", stringType)] }
     val dpInnsynAudience by lazy { properties[Key("DP_INNSYN_AUDIENCE", stringType)] }
     val dpInnsynUrl by lazy { properties[Key("DP_INNSYN_URL", stringType)] }
-
     val tokenXClient by lazy {
         val tokenX = OAuth2Config.TokenX(properties)
         CachedOauth2Client(
             tokenEndpointUrl = tokenX.tokenEndpointUrl,
-            authType = tokenX.privateKey()
+            authType = tokenX.privateKey(),
         )
     }
 
     fun tokenXClient(audience: String) = { subjectToken: String ->
         tokenXClient.tokenExchange(
-            token = subjectToken, audience = audience
+            token = subjectToken,
+            audience = audience,
         ).accessToken
     }
 }
