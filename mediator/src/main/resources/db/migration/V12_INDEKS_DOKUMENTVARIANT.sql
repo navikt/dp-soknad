534007fbb7bd7718c34b1e4913d9bdcb2c84ee9b
André Roaldseth <andre.roaldseth@nav.no>
2023-10-09 10:02:30 +0200

    Lag indekser på dokument/innsending

---
+++ mediator/src/main/resources/db/migration/V12_INDEKS_DOKUMENTVARIANT.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_dokumentvariant_dokument_uuid
+    ON dokumentvariant_v1 (dokument_uuid);
---
+++ mediator/src/main/resources/db/migration/V13_INDEKS_DOKUMENT.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_dokument_v1_innsending_uuid
+    ON dokument_v1 (innsending_uuid);
