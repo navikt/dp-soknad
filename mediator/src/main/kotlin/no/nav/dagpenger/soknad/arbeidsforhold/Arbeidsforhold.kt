bd1e62603b4f66b6b2715ec7ad393eb609d01722
celinetendam <celine.ten.dam@nav.no>
2024-01-24 10:16:15 +0100

    Flater ut feltene for arbeidsforholddetaljer i arbeidsforhold
    Co-authored-by: Abdifatah Bashi <abdifatah.bashi@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/arbeidsforhold/Arbeidsforhold.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/arbeidsforhold/Arbeidsforhold.kt
@@ -5,17 +5,13 @@ import java.time.LocalDate
 data class Arbeidsforhold(
     val id: String,
     val arbeidsgiver: Arbeidsgiver,
-    val ansettelsesdetaljer: Ansettelsesdetaljer,
     val endringsAarsak: String,
     val sluttAarsak: String,
     val startdato: LocalDate,
     val sluttdato: LocalDate,
-)
-
-data class Arbeidsgiver(val navn: String, val land: String)
-
-data class Ansettelsesdetaljer(
     val stillingsprosent: Double,
     val antallTimerPerUke: Double,
     val ansettelsesform: String,
 )
+
+data class Arbeidsgiver(val navn: String, val land: String)
