344d1a37e4d87ded4481424e027adf517c5318c6
celinetendam <celine.ten.dam@nav.no>
2024-04-09 13:43:37 +0200

    Fjerne sikkerlogging som ikke gikk til sikkerlogg😰

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/observers/SøknadLoggerObserver.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/observers/SøknadLoggerObserver.kt
@@ -5,7 +5,6 @@ import no.nav.dagpenger.soknad.SøknadObserver
 
 object SøknadLoggerObserver : SøknadObserver {
     private val log = KotlinLogging.logger { }
-    private val sikkerlogg = KotlinLogging.logger { "tjenestekall.SøknadLoggerObserver" }
 
     override fun søknadTilstandEndret(event: SøknadObserver.SøknadEndretTilstandEvent) {
         log.info {
@@ -23,12 +22,6 @@ object SøknadLoggerObserver : SøknadObserver {
     override fun søknadSlettet(event: SøknadObserver.SøknadSlettetEvent) {
         log.info {
             "Søknad ${event.søknadId} slettet"
-        }
-    }
-
-    override fun søknadInnsendt(event: SøknadObserver.SøknadInnsendtEvent) {
-        sikkerlogg.info {
-            "Søknad ${event.søknadId} innsendt på tidspunkt ${event.søknadTidspunkt} med søknaddata ${event.søknadData}"
         }
     }
 }
