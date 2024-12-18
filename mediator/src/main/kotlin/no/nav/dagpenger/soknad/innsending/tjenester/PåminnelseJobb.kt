2a0f9df058a1bda97d9bc6e1f497552adbfdaba9
Geir André Lund <geir.andre.lund@nav.no>
2024-12-18 12:38:45 +0100

    Gjør om jobben til en 1 gangs jobb på oppstart. Og at det er bare 1 pod som får lov til å gjøre det. Denne er _IKKE_ koblet inn pt.
    Co-authored-by: Celine Dam <celine.ten.dam@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/innsending/tjenester/PåminnelseJobb.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/innsending/tjenester/PåminnelseJobb.kt
@@ -6,58 +6,57 @@ import kotliquery.using
 import mu.KotlinLogging
 import no.nav.dagpenger.soknad.hendelse.innsending.InnsendingPåminnelseHendelse
 import no.nav.dagpenger.soknad.innsending.InnsendingMediator
+import no.nav.dagpenger.soknad.sletterutine.medLås
 import java.util.UUID
 import javax.sql.DataSource
-import kotlin.concurrent.fixedRateTimer
-import kotlin.time.Duration.Companion.minutes
 
-internal class PåminnelseJobb(private val innsendingMediator: InnsendingMediator, private val datasource: DataSource) {
+internal class PåminnelseJobb(
+    private val innsendingMediator: InnsendingMediator,
+    private val datasource: DataSource,
+) {
     private val logger = KotlinLogging.logger {}
+    private val låseNøkkel = 45678
 
     fun påminn() {
-        fixedRateTimer(
-            name = "Innsending påminnelse jobb",
-            daemon = true,
-            initialDelay = 1.minutes.inWholeMilliseconds,
-            period = 15.minutes.inWholeMilliseconds,
-            action = {
-                try {
-                    logger.info { "Henter innsendinger som trenger påminnelse" }
-                    val påminnelseHendelser = hentInnsendingerSomTrengerPåminnelse().mapNotNull {
-                        innsendingMediator.hent(it)?.let { innsending ->
-                            InnsendingPåminnelseHendelse(innsending.innsendingId, innsending.hentEier())
-                        }
+        try {
+            logger.info { "Henter innsendinger som trenger påminnelse" }
+            val påminnelseHendelser =
+                hentInnsendingerSomTrengerPåminnelse().mapNotNull {
+                    innsendingMediator.hent(it)?.let { innsending ->
+                        InnsendingPåminnelseHendelse(innsending.innsendingId, innsending.hentEier())
                     }
-
-                    logger.info { "Håndterer innsending påminnelse for ${påminnelseHendelser.size} innsendinger" }
-
-                    påminnelseHendelser.forEach {
-                        logger.info { "Håndterer innsending påminnelse for innsending med id ${it.innsendingId}" }
-                        innsendingMediator.behandle(it)
-                    }
-                } catch (e: Exception) {
-                    logger.error { "Påminnelse om innsendinger jobb feilet: $e" }
                 }
-            },
-        )
-    }
 
-    private fun hentInnsendingerSomTrengerPåminnelse(): List<UUID> {
-        return using(sessionOf(datasource)) { session ->
-            session.run(
-                queryOf(
-                    //language=PostgreSQL
-                    """
-                    SELECT innsending_uuid
-                    FROM innsending_v1
-                    WHERE innsendt > '2023.01.01'
-                      AND innsendt < '2024-12-18'
-                      AND tilstand = 'AvventerMidlertidligJournalføring';
-                    """.trimIndent(),
-                ).map {
-                    it.uuid("innsending_uuid")
-                }.asList,
-            )
+            logger.info { "Håndterer innsending påminnelse for ${påminnelseHendelser.size} innsendinger" }
+
+            påminnelseHendelser.forEach {
+                logger.info { "Håndterer innsending påminnelse for innsending med id ${it.innsendingId}" }
+                innsendingMediator.behandle(it)
+            }
+        } catch (e: Exception) {
+            logger.error { "Påminnelse om innsendinger jobb feilet: $e" }
         }
     }
+
+    private fun hentInnsendingerSomTrengerPåminnelse(): List<UUID> =
+        using(sessionOf(datasource)) { session ->
+            session.medLås(låseNøkkel) {
+                session.transaction { tx ->
+                    tx.run(
+                        queryOf(
+                            //language=PostgreSQL
+                            """
+                            SELECT innsending_uuid
+                            FROM innsending_v1
+                            WHERE innsendt > '2023.01.01'
+                              AND innsendt < '2024-12-18'
+                              AND tilstand = 'AvventerMidlertidligJournalføring';
+                            """.trimIndent(),
+                        ).map {
+                            it.uuid("innsending_uuid")
+                        }.asList,
+                    )
+                }
+            } ?: emptyList()
+        }
 }
