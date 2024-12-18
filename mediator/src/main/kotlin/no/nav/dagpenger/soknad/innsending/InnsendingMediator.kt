51597e9a1440a9c70cea1d081bd2d8be0be11a3f
Geir André Lund <geir.andre.lund@nav.no>
2024-12-18 11:25:49 +0100

    Midlertidig godta at vi ikke finner innsending basert på journalpost ID. Vi har sendt behov om journalføring flere ganger ved en feil.
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/soknad/innsending/InnsendingMediator.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/soknad/innsending/InnsendingMediator.kt
@@ -5,6 +5,7 @@ import no.nav.dagpenger.soknad.Aktivitetslogg
 import no.nav.dagpenger.soknad.BehovMediator
 import no.nav.dagpenger.soknad.Innsending
 import no.nav.dagpenger.soknad.InnsendingObserver
+import no.nav.dagpenger.soknad.db.DataConstraintException
 import no.nav.dagpenger.soknad.hendelse.Hendelse
 import no.nav.dagpenger.soknad.hendelse.SøknadHendelse
 import no.nav.dagpenger.soknad.hendelse.innsending.ArkiverbarSøknadMottattHendelse
@@ -65,7 +66,11 @@ internal class InnsendingMediator(
         }
     }
 
-    private fun behandle(hendelse: Hendelse, innsending: Innsending, håndter: (Innsending) -> Unit) = try {
+    private fun behandle(
+        hendelse: Hendelse,
+        innsending: Innsending,
+        håndter: (Innsending) -> Unit,
+    ) = try {
         håndter(innsending)
         innsendingRepository.lagre(innsending)
         finalize(hendelse)
@@ -82,17 +87,29 @@ internal class InnsendingMediator(
         throw e
     }
 
-    private fun behandle(hendelse: JournalførtHendelse, håndter: (Innsending) -> Unit) {
-        val innsending = innsendingRepository.hentInnsending(hendelse.journalpostId()).also { innsending ->
-            innsendingObservers.forEach { innsending.addObserver(it) }
+    private fun behandle(
+        hendelse: JournalførtHendelse,
+        håndter: (Innsending) -> Unit,
+    ) {
+        try {
+            val innsending =
+                innsendingRepository.hentInnsending(hendelse.journalpostId()).also { innsending ->
+                    innsendingObservers.forEach { innsending.addObserver(it) }
+                }
+            behandle(hendelse, innsending, håndter)
+        } catch (e: DataConstraintException) {
+            logger.info { "Fant ikke innsending basert på journalpost: ${hendelse.journalpostId()}" }
         }
-        behandle(hendelse, innsending, håndter)
     }
 
-    private fun behandle(hendelse: InnsendingHendelse, håndter: (Innsending) -> Unit) {
-        val innsending = hentEllerOpprettInnsending(hendelse).also { innsending ->
-            innsendingObservers.forEach { innsending.addObserver(it) }
-        }
+    private fun behandle(
+        hendelse: InnsendingHendelse,
+        håndter: (Innsending) -> Unit,
+    ) {
+        val innsending =
+            hentEllerOpprettInnsending(hendelse).also { innsending ->
+                innsendingObservers.forEach { innsending.addObserver(it) }
+            }
         behandle(hendelse, innsending, håndter)
     }
 
@@ -114,7 +131,11 @@ internal class InnsendingMediator(
             }
         }
 
-    private fun errorHandler(err: Exception, message: String, context: Map<String, String> = emptyMap()) {
+    private fun errorHandler(
+        err: Exception,
+        message: String,
+        context: Map<String, String> = emptyMap(),
+    ) {
         logger.error("alvorlig feil: ${err.message} (se sikkerlogg for melding)", err)
         withMDC(context) { sikkerLogger.error("alvorlig feil: ${err.message}\n\t$message", err) }
     }
@@ -126,5 +147,7 @@ internal class InnsendingMediator(
         behovMediator.håndter(hendelse)
     }
 
-    internal class SøknadIkkeFunnet(message: String) : RuntimeException(message)
+    internal class SøknadIkkeFunnet(
+        message: String,
+    ) : RuntimeException(message)
 }
